941e0005eb7b782060c689c2cac92b8563ff349f
Alice-Retz <alicegretz@gmail.com>
2021-10-05 13:25:54 -0500

    POST studio route test written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,47 @@ const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
 
+async function saveStudios() {
+  const testStudio = [
+    {
+      id: '1',
+      name: 'Blum House',
+      city: 'Hollywood',
+      state: 'California',
+      country: 'United States of America',
+    },
+  ];
+  await Promise.all(
+    testStudio.map(async (arr) => {
+      await request(app).post('/api/studios').send(arr);
+    })
+  );
+}
+
 describe('banana routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('should save a new studio', () => {
+    return request(app)
+      .post('/api/studios')
+      .send({
+        id: '1',
+        name: 'Blum House',
+        city: 'Hollywood',
+        state: 'California',
+        country: 'United States of America',
+      })
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: '1',
+          name: 'Blum House',
+          city: 'Hollywood',
+          state: 'California',
+          country: 'United States of America',
+        });
+      });
   });
 
   afterAll(() => {
