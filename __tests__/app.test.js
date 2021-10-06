0fcf1815e94b1242c2e23a5ceba5f470435d577a
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-10-06 14:29:05 -0700

    pushing changes and select id of studios and the films associated test is passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -24,8 +24,18 @@ async function saveFilms() {
     {
       title: 'Lion King',
       studioId: 1,
-      released: 1994
+      released: 1994,
     },
+    {
+      title: 'Babe',
+      studioId: 1,
+      released: 1991
+    },
+    {
+      title: 'Babe II Pig in the City',
+      studioId: 1,
+      released: 1992
+    }
   ];
   await Promise.all(
     testFilm.map(async (arr) => {
@@ -60,46 +70,37 @@ describe('banana routes', () => {
       });
   });
 
-
   ///---GET STUDIO ID & NAME---///
-  it('should return all studios names and ideeess', async () =>
-  {
+  it('should return all studios names and ideeess', async () => {
     await saveStudios();
     return request(app)
       .get('/api/studios')
-      .then((res) =>
-      {
+      .then((res) => {
         // console.log('AT GET STUDIO NAME AND ID TEST', res.body);
-        expect(res.body).toEqual(
-          [{
+        expect(res.body).toEqual([
+          {
             id: '1',
             name: 'Blum House',
-          }]
-        );
+          },
+        ]);
       });
   });
 
-  it.only('should return a studio with all films and titles', async () =>
-  {
+  it.only('should return a studio with all films and titles', async () => {
     await saveStudios();
     await saveFilms();
     return request(app)
       .get('/api/studios/1')
-      .then((res) =>
-      {
+      .then((res) => {
         // console.log('AT GET STUDIO FILM AND TITLE TEST', res.body);
-        expect(res.body).toEqual(
-          {
-            id: '1',
-            name: 'Blum House',
-            city: 'Hollywood',
-            state: 'California',
-            country: 'United States of America',
-            film_id: 1,
-            title: 'Lion King' 
-          
-          }
-        );
+        expect(res.body).toEqual({
+          id: '1',
+          name: 'Blum House',
+          city: 'Hollywood',
+          state: 'California',
+          country: 'United States of America',
+          films: [{ id: '1', title: expect.any(String) }, { id: '2', title: expect.any(String) }, { id: '3', title: expect.any(String) }],
+        });
       });
   });
 
